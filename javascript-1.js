9e229f42a80f38a9edcf2c2e340f6da6940edb54
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-01-16 11:15:02 -0800

    Add calculate function for mathematical calculations

---
+++ javascript-1.js
@@ -0,0 +1,29 @@
+/*
+@param  firstOperand - number
+@param secondOperand - number  
+@param operator - string representing a mathematical operator
+@return result - string representing result of math operation
+*/
+
+function calculate(firstOperand, operator, secondOperand) {
+    let result = "";
+    switch(operator) {
+        case "+" : result = `Sum of ${firstOperand} and ${secondOperand} is: ${firstOperand + secondOperand}`;
+                    break;
+        case "*" : result = `Product of ${firstOperand} and ${secondOperand} is: ${firstOperand * secondOperand}`;
+                    break;
+        case "/" : result = `Quotient of ${firstOperand} / ${secondOperand} is: ${firstOperand / secondOperand}`;
+                    break;
+        case "-" : result = `Difference between ${firstOperand} and ${secondOperand} is: ${firstOperand - secondOperand}`;
+                    break;
+        case "%" : result = `Reminder of ${firstOperand} and ${secondOperand} is: ${firstOperand % secondOperand}`;
+                    break;
+        case "min" : result = `Minimum of ${firstOperand} and ${secondOperand} is: ${Math.min(firstOperand, secondOperand)}`;
+                    break;
+        case "max" : result = `Maximum of ${firstOperand} and ${secondOperand} is: ${Math.max(firstOperand, secondOperand)}`;
+                    break;
+        default : result = "Invalid operation or operands";
+    }
+    return result;
+}
+
