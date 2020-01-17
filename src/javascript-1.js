d26bfeeca54c85bbab22cb6c5e6297214ab068d5
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-01-16 16:42:21 -0800

    Add function to print an array and its length

--- src/javascript-1.js
+++ src/javascript-1.js
@@ -1,8 +1,9 @@
 /*
-@param  firstOperand - number
-@param secondOperand - number  
-@param operator - string representing a mathematical operator
-@return result - string representing result of math operation
+* Performs mathematical operation on the two operand parameters, based on the operator argument. 
+* @param  firstOperand - number
+* @param secondOperand - number  
+* @param operator - string representing a mathematical operator
+* @return result - string representing result of math operation
 */
 
 function calculate(firstOperand, operator, secondOperand) {
@@ -23,7 +24,7 @@ function calculate(firstOperand, operator, secondOperand) {
         case "max" : result = `Maximum of ${firstOperand} and ${secondOperand} is: ${Math.max(firstOperand, secondOperand)}`;
                     break;
         default : result = "Invalid operation or operands";
-    }
+    } 
     return result;
 }
 
@@ -32,8 +33,34 @@ function calculate(firstOperand, operator, secondOperand) {
 *
 * @param first - integer
 * @param second - integer
-* @result integer     
+* @return integer     
 */
 function squareMax(first, second) {
     return ((Math.pow(first,2)) > (Math.pow(second,2)))? first : second; 
 }
+
+
+/*
+* Performs array operations and prints an array and its length.
+* @param array
+* @return array
+*/
+function printCuteAnimals(animals){
+    let cuteAnimals = [];
+    for (const index in animals) {
+        if(index % 2 == 0) {
+            cuteAnimals.push(animals[index]);            
+        }
+    }
+    console.log(`cute animals are: ${cuteAnimals}\n`);
+    console.log(`Animals are : ${animals.join(" ")}.\n And its length is : ${animals.length}\n`);
+    animals.pop();
+    console.log(`Animals after popping an animal out are : ${animals.join(" ")}.\n And its length is ${animals.length}\n`);
+    const concatinatedAnimals = animals.concat(['rabbit', 'panda', 'tiger']);
+    console.log(`Conactinated Animals, stored to a another variable : \n${concatinatedAnimals.join(" ")}.\n And its length is ${concatinatedAnimals.length}\n`);
+    console.log(`Animals after concatinating more animals to it : \n${animals.join(" ")}.\n And its length is ${animals.length}\n`);
+    return animals;
+}
+
+
+
