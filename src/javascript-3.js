bccf85c7084685563a069f0cad54f0ceb80e4980
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-01-21 09:51:08 -0800

    Add solution to HAckerRank if-else challenge

--- src/javascript-3.js
+++ src/javascript-3.js
@@ -7,7 +7,7 @@
 - David loves popcorn and will go to the movies under any circumstance
 - Alex will automatically go to the movies if David goes, and will automatically not go to the movies if David does not go
 - Ming will not attend if David has already said he is attending and Ming will say yes if Priya says yes.
-- Breanna loves to be around people and will only go if there are at least 2 others going
+-Breanna loves to be around people and will only go if there are at least 2 others going
 */
 function movieNight(friends) {
     let movieGoers = [];
@@ -18,9 +18,26 @@ function movieNight(friends) {
     if (friends.includes("David")) {
         movieGoers.push("David", "Alex");
     }
-    if(movieGoers.length >= 2) {
+    if (movieGoers.length >= 2) {
         movieGoers.push("Breanne");
     }
     return movieGoers;
+}
+
+/*
+Takes in a score and generates a grade based on the conditions
+@param score - number
+@result grade - number
+*/
+function getGrade(score) {
+    let grade;
+    if (score > 25 && score <= 30) grade = 'A';
+    else if (score > 20 && score <= 25) grade = 'B';
+    else if (score > 15 && score <= 20) grade = 'C';
+    else if (score > 10 && score <= 15) grade = 'D';
+    else if (score > 5 && score <= 10) grade = 'E';
+    else if (score > 0 && score <= 5) grade = 'F';
+    else grade = 'Invalid score';
+    return grade;
 }
 
