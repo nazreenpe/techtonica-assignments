cbc29bd849c77270533bc26da84c2ef773892481
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-01-23 10:44:24 -0800

    Add solution for looping problem

--- src/basic-javascript-practice.js
+++ src/basic-javascript-practice.js
@@ -3,11 +3,26 @@ Takes an input of the number of meters and outputs the result of the number of i
 @param meters - number
 @return number
 */
-
 function meterToInches(meters) {
     const oneMeterInInches = 39.3701;
     return meters * oneMeterInInches;
 }
+
+/*
+Takes an argument that is a number, loops through and displays a message “Hello World”
+ that number of times.
+ @param number n
+*/
+function loopThrough(n) {
+    for (let i = 0; i < n; i++) {
+        console.log("Hello World");
+    }
+}
+
+
+
+
+
 
 /*
 Logs results of functions
@@ -16,7 +31,10 @@ function main() {
     let meters = 15.6;
     const inches = meterToInches(meters);
     console.log(`${meters} meters = ${inches} inches`);
-
+    console.log();
+    let n = 4;
+    loopThrough(n);
+    
 
 }
 
