2a9f1ca7fd941efeb6fd3dc0e15f8db1c66e4804
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-02-04 13:08:33 -0800

    Add eventRecommender js file

---
+++ eventonica/src/eventRecommender.js
@@ -0,0 +1,37 @@
+class EventRecommender {
+    constructor() {
+    // All main properties should go here.
+    
+    this.events = [];
+    this.users = [];
+    
+    }
+
+    addEvent() {
+    // Adds a new Event to the System
+    }
+
+    addUser() {
+    // Adds a new User to the System
+    }
+
+    saveUserEvent(){
+    // Allow users to save events to a personal Events array.
+    }
+
+    deleteUser() {
+    // Deletes a User from the system
+    }
+   
+    deleteEvent() {
+    // Deletes the Event from the system
+    }
+
+    findEventsByDate(){
+    // Returns all events on a given date
+    }
+    
+    findEventsbyCategory(){
+    // Returns all events in a given category
+    }
+}
