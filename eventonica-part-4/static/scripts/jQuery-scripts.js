63899a243ee3b2905b06e935066f2dc874f90f3d
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-02-28 15:22:08 -0800

    Add Ticketmaster API in eventonica. Add jquery to show the results as well

--- eventonica-part-4/static/scripts/jQuery-scripts.js
+++ eventonica-part-4/static/scripts/jQuery-scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     const eventRecommender = new EventRecommender();
- 
+
     function refreshUsers(elementId) {
         let htmlInput = "";
         $.each(eventRecommender.users, (userId) => {
@@ -19,7 +19,7 @@ $(document).ready(() => {
         $(`#${elementId}`).html(htmlInput);
     }
 
-    function refreshUserEvents(elementId){
+    function refreshUserEvents(elementId) {
         let htmlInput = "";
         $.each(eventRecommender.userEvents, (userId) => {
             let eventIds = eventRecommender.userEvents[userId];
@@ -41,12 +41,12 @@ $(document).ready(() => {
         this.reset();
     });
 
-    $("#delete-user").submit(function(e) {
+    $("#delete-user").submit(function (e) {
         let id = $("#delete-user-id").val();
         let password = $("#delete-password").val();
         if (eventRecommender.deleteUser(id, password)) {
-          refreshUsers("all-users");
-          $("#user-error-message").remove();
+            refreshUsers("all-users");
+            $("#user-error-message").remove();
         } else {
             $("#user-error-message").text("Please enter a valid userID and password");
         }
@@ -54,7 +54,7 @@ $(document).ready(() => {
         this.reset();
     })
 
-    $("#add-event").submit(function (e) { 
+    $("#add-event").submit(function (e) {
         let name = $("#add-event-name").val();
         let date = $("#add-event-date").val();
         let category = $("#add-category").val();
@@ -65,11 +65,11 @@ $(document).ready(() => {
         this.reset();
     });
 
-     $("#delete-event").submit(function(e) {
+    $("#delete-event").submit(function (e) {
         let id = $("#delete-event-id").val();
         if (eventRecommender.deleteEvent(id)) {
             refreshEvents("all-events")
-           $("#delete-event-error").remove();
+            $("#delete-event-error").remove();
         } else {
             $("#delete-event-error").text("Please enter a valid eventID");
         }
@@ -77,7 +77,36 @@ $(document).ready(() => {
         this.reset();
     })
 
-    $("#date-search").submit(function (e) { 
+    $("#tm-event-search").submit(function (e) {
+        e.preventDefault();
+
+        let keyword = $("#tm-event-search-keyword").val();
+        $.ajax({
+            type: "GET",
+            url: `https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&keyword=${keyword}`,
+
+            async: true,
+            dataType: "json",
+            success: function (json) {
+                console.log(json);
+                json._embedded.events.forEach(event => {
+                    let name = event.name;
+                    let category = event.type;
+                    let date = moment(new Date(event.dates.start.dateTime)).format("MM/DD/YYYY");
+
+                    let newEvent = new Event(name, date, category);
+                    eventRecommender.addEvent(newEvent);
+                });
+                refreshEvents("all-events");
+            },
+            error: function (xhr, status, err) {
+                console.log("Error fecthing data from Ticketmaster")
+            }
+        });
+
+    });
+
+    $("#date-search").submit(function (e) {
         let date = $("#date-search-id").val();
         let events = eventRecommender.findEventsByDate(date);
         let displayEventsByDateHtml = "";
@@ -85,11 +114,11 @@ $(document).ready(() => {
             displayEventsByDateHtml += `<li id="${event.eId}"> Name: ${event.eventName} Date: ${event.date} Category: ${event.category} Id: ${event.eId}</li>`;
         }
         $("#events-by-date").html(displayEventsByDateHtml);
-        
+
         e.preventDefault();
     });
-    
-    $("#category-search").submit(function (e) { 
+
+    $("#category-search").submit(function (e) {
         let category = $("#category-search-id").val();
         let events = eventRecommender.findEventsByCategory(category);
         let displayEventsByDateHtml = "";
@@ -97,16 +126,16 @@ $(document).ready(() => {
             displayEventsByDateHtml += `<li id="${event.eId}"> Name: ${event.eventName} Date: ${event.date} Category: ${event.category} Id: ${event.eId}</li>`;
         }
         $("#events-by-category").html(displayEventsByDateHtml);
-        
+
         e.preventDefault();
     });
 
-    $("#save-user-event").submit(function(e) {
+    $("#save-user-event").submit(function (e) {
         let eventId = $("#save-event-id").val();
         let userId = $("#save-user-id").val();
         if (eventRecommender.saveUserEvent(userId, eventId)) {
             refreshUserEvents("event-signups");
-        } else{
+        } else {
             console.log("Error: EventId/ userId doesn't exist");
         }
         e.preventDefault();
